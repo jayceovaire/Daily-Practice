8c70184a1fdf5dfd9429d3fbd63524d2f8fd52d6
Jayceovaire <joshuaovermann@gmail.com>
2022-12-05 17:47:53 -0500

    Made the results print look nice and removed self tests from source code

--- Coin_toss_simulator.py
+++ Coin_toss_simulator.py
@@ -1,24 +1,35 @@
 import random
 
-#Coin Toss Simulator
+# Coin Toss Simulator
+
 
 class Coin:
     def __init__(self):
         pass
-    def toss(self, num_tosses):
+
+    @staticmethod
+    def toss(num_tosses=1):
+        # results tallies outcomes of flips stored in outcomes list.
         results = {'Heads': 0, 'Tails': 0}
         possibilties = ['Heads', 'Tails']
         outcomes = []
+        # counter is equal to number of times you want to flip coin
         counter = int(num_tosses)
+
         while counter != 0:
+            # Flips coin number of times as input (num_tosses)
             outcome = random.choice(possibilties)
             outcomes.append(outcome)
             counter -= 1
+        # total flips reduced by one and outcome recorded in outcomes
+        # flip outcomes are tallied in results list above.
+
         for i in outcomes:
             if i == 'Heads':
                 results[i] = results.get(i, 0) + 1
             if i == 'Tails':
                 results[i] = results.get(i, 0) + 1
-        print(results)
-coin1 = Coin()
-coin1.toss(1)
+
+        # Display results of Heads and Tails
+        for key, value in results.items():
+            print(key, ":", value)
