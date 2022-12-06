f5c2e2143ad1751ce7908dc5cdfbbf3af0e7d6f3
Jayceovaire <joshuaovermann@gmail.com>
2022-12-06 15:53:27 -0500

    fixed typo in possibilities variable and shortened "results" for loop.

--- Coin_toss_simulator.py
+++ Coin_toss_simulator.py
@@ -1,8 +1,5 @@
 import random
 
-# Coin Toss Simulator
-
-
 class Coin:
     def __init__(self):
         pass
@@ -11,24 +8,21 @@ class Coin:
     def toss(num_tosses=1):
         # results tallies outcomes of flips stored in outcomes list.
         results = {'Heads': 0, 'Tails': 0}
-        possibilties = ['Heads', 'Tails']
+        possibilities = ['Heads', 'Tails']
         outcomes = []
         # counter is equal to number of times you want to flip coin
         counter = int(num_tosses)
 
         while counter != 0:
             # Flips coin number of times as input (num_tosses)
-            outcome = random.choice(possibilties)
+            outcome = random.choice(possibilities)
             outcomes.append(outcome)
             counter -= 1
         # total flips reduced by one and outcome recorded in outcomes
         # flip outcomes are tallied in results list above.
 
         for i in outcomes:
-            if i == 'Heads':
-                results[i] = results.get(i, 0) + 1
-            if i == 'Tails':
-                results[i] = results.get(i, 0) + 1
+            results[i] = results.get(i, 0) + 1
 
         # Display results of Heads and Tails
         for key, value in results.items():
