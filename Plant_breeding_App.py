31dcc1db31e0dc3b4ad1e14175b626b20e07ba02
Jayceovaire <joshuaovermann@gmail.com>
2023-01-19 20:04:32 -0500

    added textbox, added reset button, breeding button fixed,

--- Plant_breeding_App.py
+++ Plant_breeding_App.py
@@ -3,7 +3,7 @@ import customtkinter
 from PIL import Image, ImageTk
 import random
 
-customtkinter.set_appearance_mode("system")
+customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("green")
 root = customtkinter.CTk()
 
@@ -16,17 +16,17 @@ plant_label2 = customtkinter.CTkLabel(master=root, pady=5, padx=100, text="Plant
 plant_label2.grid(row=2, column=0)
 
 color_label = customtkinter.CTkLabel(master=root, text="Plant Color")
-color_label.grid(row=0, column=4, pady=5, padx=10)
+color_label.grid(row=0, column=1, pady=5, padx=10)
 color_button1 = customtkinter.CTkSegmentedButton(master=root, values=["Green", "Purple"])
-color_button1.grid(row=1, column=4, pady=5, padx=10)
-color_button2 = customtkinter.CTkSegmentedButton(master=root, values=["Green","Purple"])
-color_button2.grid(row=2, column=4, pady=5, padx=10)
+color_button1.grid(row=1, column=1, pady=5, padx=10)
+color_button2 = customtkinter.CTkSegmentedButton(master=root, values=["Green", "Purple"])
+color_button2.grid(row=2, column=1, pady=5, padx=10)
 
 height_label = customtkinter.CTkLabel(master=root, text="Plant Height")
 height_label.grid(row=0, column=5, pady=5, padx=10)
 height_button1 = customtkinter.CTkSegmentedButton(master=root, values=["Tall", "Short"])
 height_button1.grid(row=1, column=5, pady=5, padx=10)
-height_button2 = customtkinter.CTkSegmentedButton(master=root, values=["Tall","Short"])
+height_button2 = customtkinter.CTkSegmentedButton(master=root, values=["Tall", "Short"])
 height_button2.grid(row=2, column=5, pady=5, padx=10)
 
 scent_label = customtkinter.CTkLabel(master=root, text="Plant Scent")
@@ -162,19 +162,28 @@ smell_counts = {"Scent1": 0, "Scent2": 0, "new smell": 0}
 color_counts = {"Green": 0, "Purple": 0}
 flower_counts = {"Heavy": 0, "Medium": 0, "Light": 0}
 height_counts = {"Tall": 0, "Short": 0}
-
+colors = []
+scents = []
+flowers = []
+strains = []
+heights = []
 
 
 
 def breed_hundred_plants():
     for i in range(100):
         breed_plants()
+    display_outcomes()
+    disable_breed_button()
+
 def display_outcomes():
     # Calculate total number of plants
     total_plants = sum(trait_counts.values())
     for trait_string, count in trait_counts.items():
         percentage = 100 * count / total_plants
-        print(f"{percentage:.2f}% of plants have traits: {trait_string}")
+        if percentage != 0.00:
+            outcome_box.insert(0.0, text=f"{percentage:.2f}% of plants have traits: {trait_string}\n")
+
 
     # iterate through plants and update count of expected strain types
     for plant in plants:
@@ -188,36 +197,93 @@ def display_outcomes():
     for color, count in color_counts.items():
         percentage = 100 * count / total_plants
         if percentage != 0.00:
-            print(f"{percentage:.2f}% of plants are {color}")
+            outcome_box.insert(0.0, text=f"{percentage:.2f}% of plants are {color}\n")
 
     for height, count in height_counts.items():
         percentage = 100 * count / total_plants
         if percentage != 0.00:
-            print(f"{percentage:.2f}% of plants are {height}")
+            outcome_box.insert(0.0, text=f"{percentage:.2f}% of plants are {height}\n")
 
     for smell, count in smell_counts.items():
         percentage = 100 * count / total_plants
         if percentage != 0.00:
-            print(f"{percentage:.2f}% of plants smell {smell}")
+            outcome_box.insert(0.0, text=f"{percentage:.2f}% of plants smell {smell}\n")
 
     for flower, count in flower_counts.items():
         percentage = 100 * count / total_plants
         if percentage != 0.00:
-            print(f"{percentage:.2f}% of plant flowers are {flower}")
+            outcome_box.insert(0.0, text=f"{percentage:.2f}% of plant flowers are {flower}\n")
 
     for strain, count in strain_counts.items():
         percentage = 100 * count / total_plants
         if percentage != 0.00:
-            print(f"{percentage:.2f}% of plants are {strain}")
+            outcome_box.insert(0.0, text=f"{percentage:.2f}% of plants are {strain}\n")
 
-def Pprint():
-    display_outcomes()
+def reset_breed_button():
+    breed_button.configure(state="normal")
+
+def reset_strains():
+    for key in strain_counts:
+        strain_counts[key] = 0
+
+def reset_colors():
+    for key in color_counts:
+        color_counts[key] = 0
+
+def reset_heights():
+    for key in height_counts:
+        height_counts[key] = 0
+
+def reset_flowers():
+    for key in flower_counts:
+        flower_counts[key] = 0
+
+def reset_scents():
+    for key in smell_counts:
+        smell_counts[key] = 0
+
+def reset_traits():
+    for key in trait_counts:
+        trait_counts[key] = 0
+
+def reset_plants():
+    plants.clear()
+    colors.clear()
+    heights.clear()
+    flowers.clear()
+    scents.clear()
+    strains.clear()
+
+
+def reset_textbox():
+    outcome_box.delete("0.0", "end")
+
+
+def reset_all():
+    reset_textbox()
+    reset_plants()
+    reset_strains()
+    reset_traits()
+    reset_scents()
+    reset_colors()
+    reset_heights()
+    reset_flowers()
+    reset_breed_button()
+    print(plants)
+    print(strain_counts)
+    print(trait_counts)
 
 breed_button = customtkinter.CTkButton(master=root, text="Breed Plants", command=breed_hundred_plants)
-breed_button.grid(row=4, column=6, pady=(50, 0), padx=10)
+breed_button.place(relx=0.42, rely=0.3)
 
-printbutton = customtkinter.CTkButton(master=root, text="Print", command=Pprint)
-printbutton.grid(row=5, column=6, pady=5, padx=10)
+def disable_breed_button():
+    breed_button.configure(state="disabled")
+
+outcome_box= customtkinter.CTkTextbox(master=root, width=600, height=300)
+outcome_box.place(relx=0.5, rely=0.7, anchor=CENTER)
+
+reset_button = customtkinter.CTkButton(master=root, text="Reset", command=reset_all)
+reset_button.place(relx=.42, rely=.35)
 
 if __name__ == "__main__":
     root.mainloop()
