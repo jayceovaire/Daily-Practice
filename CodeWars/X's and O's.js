ceaae943c4140dddeede26d4a120ac485f17fe22
jayceovaire <118041687+jayceovaire@users.noreply.github.com>
2023-03-28 19:37:21 -0400

    7kyu Problem - Evaluating number of characters in a string.

---
+++ CodeWars/X's and O's.js
@@ -0,0 +1,32 @@
+// Check to see if a string has the same amount of Xs and Os.
+// The method must return a boolean and be case-insensitive.
+// The string can contain any char.
+
+function XO(str) {
+    let copy = str.slice()
+    let numX = 0
+    let numO = 0
+    for(let i = 0; i < copy.length; i++){
+        if(copy[i].toLowerCase() === 'x'){
+            numX += 1
+        }
+        else if(copy[i].toLowerCase() === 'o'){
+            numO += 1
+        }
+    }
+    if(numX === numO){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+console.log(XO("afa")) //expect true X = 0 O = 0
+console.log(XO('xo')) //expect true X = O
+console.log(XO('xoxx')) //expect false X != O
+
+
+
+
+
