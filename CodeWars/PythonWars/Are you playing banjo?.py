2f8d359efa594985d8aee1538f6b6143fede4626
Jayceovaire <joshuaovermann@gmail.com>
2023-04-25 20:09:42 -0400

    8kyu problem - given a name, return name + plays banjo if their name starts with an R or r

---
+++ CodeWars/PythonWars/Are you playing banjo?.py
@@ -0,0 +1,13 @@
+
+# given a name, return name + plays banjo if their name starts with an R or r
+
+def are_you_playing_banjo(name):
+    if name[0] == 'R' or name[0] == 'r':
+        return f"{name} plays banjo"
+    else:
+        return f"{name} does not play banjo"
+
+
+letter = 'stringy'
+
+letter = letter.lo
