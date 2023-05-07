1796ffb20572bdd71d75470b770bf83000394b5f
jayceovaire <118041687+jayceovaire@users.noreply.github.com>
2023-05-06 21:09:41 -0400

    Added linked List interface

---
+++ FrontEnd Masters - Primagen Algorithms/Linked List.ts
@@ -0,0 +1,16 @@
+
+// Linked Lists are essentially a Tree / graph
+
+interface LinkedList<T> {
+    get_length(): number;
+    insertAt(item: T, index: number): void;
+    remove(item: T): T | undefined;
+    removeAt(index: number): T | undefined;
+    append(item: T): void;
+    prepend(item: T): void;
+    get(index: number): T | undefined;
+}
+
+
+
+
