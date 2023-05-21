22e6803e033517090442036b2e87430cd9c8db6e
jayceovaire <118041687+jayceovaire@users.noreply.github.com>
2023-05-21 18:47:19 -0400

    8kyu problem - Given a distance to travel as well as the mpg economy of a car and the fuel left in the tank write a function that returns True if you can make it to your destination with the current fuelLeft

---
+++ CodeWars/TypescriptWars/Will you make it.ts
@@ -0,0 +1,8 @@
+
+// Given a distance to travel as well as the mpg economy of a car and the fuel left in the tank
+// write a function that returns True if you can make it to your destination with the current fuelLeft
+
+
+export const zeroFuel = (distance: number, mpg: number, fuelLeft: number): boolean => {
+    return distance <= (mpg * fuelLeft)
+}
